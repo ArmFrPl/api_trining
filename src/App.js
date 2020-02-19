97d4372d56d4e3d0cc9f19ec31c295c745d7a542
ArmFrPl <ehakobkekhvyan7@gmail.com>
2020-02-19 10:46:54 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,90 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import unsplash from '../src/api/unsplash';
+import 'bootstrap/dist/css/bootstrap.css';
+import {apiUrl} from "./defVars";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    planets: [],
+    resident: []
+  };
+
+  onSubmit = async (term) => {
+    const response = await unsplash.get(`${apiUrl}/planets`, {
+      params: {query: term}
+    });
+    this.setState({planets: response.data.results})
+  };
+
+  getResidents = async (term) => {
+    // const resp = await unsplash.get(`${this.state.planets.map(i => {
+    //   if(i.residents){
+    //     return i.residents[0];
+    //   }
+    // })}`, {
+    //   params: {query: term}
+    // });
+    const resp = await unsplash.get(`https://swapi.co/api/people/5/`, {
+      params: {query: term}
+    });
+    console.log(resp.data);
+    this.setState({resident: resp.data})
+  };
+
+  render(){
+    let cont;
+    // if(this.state.resident){
+      // cont = this.state.resident.map(it => {
+      // console.log(this.state.resident)
+
+      // let it = this.state.resident[0];
+      // return <tr>
+        {/*<td scope="row">{it.name}</td>*/}
+        {/*<td onClick={this.getResidents}>{it.height}</td>*/}
+        {/*<td>{it.mass}</td>*/}
+        {/*<td>{it.gender}</td>*/}
+        {/*<td>{it.hair_color}</td>*/}
+        {/*<td>{it.skin_color}</td>*/}
+        {/*<td>{it.eye_color}</td>*/}
+        {/*<td>{it.birth_year}</td>*/}
+      // </tr>
+      // });
+    // }else{
+      cont = this.state.planets.map(it => {
+        return <tr>
+          <td scope="row">{it.name}</td>
+          <td onClick={this.getResidents}>{it.population}</td>
+          <td>{it.terrain}</td>
+          <td>{it.climate}</td>
+          <td>{it.rotation_period}</td>
+          <td>{it.orbital_period}</td>
+        </tr>
+      });
+    // }
+
+
+    return (
+      <div className="App">
+        <table className='table table-bordered table-dark' style={{marginTop: '10px'}}>
+          <thead>
+          <tr>
+            <th scope="col">Planet title</th>
+            <th scope="col">Planet Population</th>
+            <th scope="col">Planet Terrain</th>
+            <th scope="col">Planet Climate</th>
+            <th scope="col">Planet Rotation Period</th>
+            <th scope="col">Planet Orbital Period</th>
+          </tr>
+          </thead>
+          <tbody>
+          {cont}
+          </tbody>
+        </table>
+        <button onClick={this.onSubmit} className="btn btn-secondary" style={{margin: "auto", display: 'flex'}}>Click me to get data!</button>
+      </div>
+    )
+  }
+
 }
 
 export default App;
---
+++ src/api/unsplash.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+import {apiUrl} from "../defVars";
+
+export default axios.create({
+  baseURL: ``
+})
